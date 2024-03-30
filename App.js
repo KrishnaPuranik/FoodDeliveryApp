947930f199e7763289a09337a58c141f5b2c2bf4
KrishnaPuranik <krishnamoorthi.puranik@gmail.com>
2024-03-30 18:08:24 +0530

    Chapter-01

---
+++ App.js
@@ -0,0 +1,14 @@
+// const reactHeading = React.createElement('h1', {id:"Heading"}, "Hello World from React!");
+// 'parent' is not HTML element, its react object. DOM will create html tag.
+const parent = React.createElement(
+    'div', 
+    {id: 'parent'}, 
+    React.createElement(
+        'div', 
+        {id: 'child'}, 
+        [React.createElement('h1', {}, "I'm an h1 tag"), React.createElement('h2', {}, "I'm an h2 tag")]
+    )
+);
+console.log(parent);
+const reactRoot = ReactDOM.createRoot(document.getElementById('root'));
+reactRoot.render(parent);
